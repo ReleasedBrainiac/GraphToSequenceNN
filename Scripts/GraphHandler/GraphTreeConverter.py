0087d6b3ac20ee31095b8ed34663581eeda0bbad
TTurke <tobias.turke@gmx.net>
2018-08-28 16:10:15 +0200

    added placeholder functions

--- Scripts/GraphHandler/GraphTreeConverter.py
+++ Scripts/GraphHandler/GraphTreeConverter.py
@@ -2,3 +2,20 @@
 '''
     This class allow to convert GraphTree's of type AnyNode from anytree library into a matrix representation.
 '''
+
+#==             Network dataset preprocessing             ==#
+# This function 
+#
+#   Inputs:
+#       data_pairs  => list of data pairs from GetSingleDatasetPair function
+#
+#   Return:
+#      
+def ConvertAnyTreeToTensorMatrices(data_pair):
+    print('This function is work in progress! [ConvertAnyTreeToTensorMatrices]')
+    return None
+
+
+def GetDataSet(data_pairs):
+    print('This function is work in progress! [GetDataSet]')
+    return None

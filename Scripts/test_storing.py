7cfa7c6ba459e928407d4da2cc2630412e590664
TTurke <tobias.turke@gmx.net>
2018-08-28 16:25:02 +0200

    test flags

--- Scripts/test_storing.py
+++ Scripts/test_storing.py
@@ -7,6 +7,6 @@ max_length = -1
 output_extender = 'dc.ouput'
 inpath = '../Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-dev.txt'
 save_as_arm = False
-print_activated_anynode = True  # this wont work if you only store arm cause no anynode is calced!
+print_activated_anynode = False  # this wont work if you only store arm cause no anynode is calced!
 
 Pipe(inpath, output_extender, max_length, save_as_arm, print_activated_anynode)

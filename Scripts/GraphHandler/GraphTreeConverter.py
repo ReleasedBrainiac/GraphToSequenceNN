3d6daba87081a3cbdd8290542fd1f1d67e1fba91
Tobias Turke <Tobias.Turke@gmx.net>
2018-08-29 15:35:31 +0200

    added placeholder functions and edited GetDataset function a little

--- Scripts/GraphHandler/GraphTreeConverter.py
+++ Scripts/GraphHandler/GraphTreeConverter.py
@@ -1,21 +1,58 @@
 # - *- coding: utf-8*-
+import numpy as np
+from TextFormatting.ContentSupport import GetType
+from TextFormatting.ContentSupport import isAnyNode, isNone, isNotNone, isList
 '''
     This class allow to convert GraphTree's of type AnyNode from anytree library into a matrix representation.
 '''
 
-#==             Network dataset preprocessing             ==#
-# This function 
-#
-#   Inputs:
-#       data_pairs  => list of data pairs from GetSingleDatasetPair function
-#
-#   Return:
-#      
-def ConvertAnyTreeToTensorMatrices(data_pair):
-    print('This function is work in progress! [ConvertAnyTreeToTensorMatrices]')
-    return None
+def GetNodesLables(tree_graph):
+    if isNotNone(tree_graph) and isAnyNode(tree_graph):
+        print('This function is work in progress! [GetGloVeFromNodesLables]')
+        return None
+    else:
+        print('WRONG INPUT FOR [GetGloVeFromNodesLables]')
+        return None
 
+def GetNeighbourhoodMatrices(tree_graph):
+    if isNotNone(tree_graph) and isAnyNode(tree_graph):
+        print('This function is work in progress! [GetNeighbourhoodMatrices]')
+        return None
+    else:
+        print('WRONG INPUT FOR [GetNeighbourhoodMatrices]')
+        return None
+
+def GetTensorMatricesFromGraphTree(tree_graph):
+    if isNotNone(tree_graph) and isAnyNode(tree_graph):
+        print('This function is work in progress! [GetTensorMatricesFromGraphTree]')
+        return None
+    else:
+        print('WRONG INPUT FOR [GetTensorMatricesFromGraphTree]')
+        return None
 
 def GetDataSet(data_pairs):
-    print('This function is work in progress! [GetDataSet]')
-    return None
+    if isNotNone(data_pairs) and isList(data_pairs):
+        data_pair_edge_lists = []
+        data_pair_features = []
+
+        for _ in range(len(data_pairs)):
+            sentence = data_pairs[0][0]
+            semantic = data_pairs[0][1]
+            edge_lists = GetTensorMatricesFromGraphTree(semantic)
+
+            data_pair_edge_lists.append(edge_lists)
+            data_pair_features.append(sentence)
+
+            print('General typ: ',GetType(data_pairs))
+            print('Sentence typ: ',GetType(sentence))
+            print('Semantic typ: ',GetType(semantic))
+
+        print(data_pair_edge_lists[0])
+        print('This function is work in progress! [GetDataSet]')
+        return None
+    else:
+        print('WRONG INPUT FOR [GetDataSet]')
+        return None
+
+
+    

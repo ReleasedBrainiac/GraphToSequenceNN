857a810bc2a3c341c9fcb0868ed8f68126405ce2
root <root@localhost.localdomain>
2018-09-03 18:06:54 +0200

    implemented my very own test system

---
+++ Scripts/TestHandler/TestCores.py
@@ -0,0 +1,45 @@
+# - *- coding: utf-8*-
+from TextFormatting.ContentSupport import isNotNone, isList, isBool, isStr
+
+def ReportTestProgress(check_value, test_name):
+    print('Assert', test_name, '!')
+    if check_value: 
+        return True
+    else:
+        return False
+
+def RunTests(class_name):
+    functions = dir(class_name)
+    TestCases = []
+
+    for func in functions:
+        if '__' not in func:
+            TestCases.append([getattr(class_name, func)(), func])  
+    
+    return TestCases
+
+def EvaluateTests(TestResults):
+    ALL_TEST_SUCCEED = True
+    ALL_FAILED_TEST = []
+
+    if isNotNone(TestResults) and isList(TestResults):
+        for elem, name in TestResults:
+            if isNotNone(elem) and isBool(elem) and isNotNone(name) and isStr(name):
+                if not elem:
+                    ALL_FAILED_TEST.append([elem, name])
+                    ALL_TEST_SUCCEED = False
+            else:
+                print('WRONG CONTENT FOUND IN LIST AT [EvaluateTests]')
+                return None
+
+        if ALL_TEST_SUCCEED:
+            print('ALL TESTS SUCCEEDED!')
+            return True
+        else:
+            for result, name in ALL_FAILED_TEST:
+                print('TEST [',name, '] RETURNED [',result, ']!')
+            return False
+
+    else:
+        print('WRONG INPUT FOR [EvaluateTests]')
+        return None

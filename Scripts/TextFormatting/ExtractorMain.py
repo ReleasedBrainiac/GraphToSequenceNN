efe2f62928bcf4809bf2539377a4ca3b6cd14ccf
ReleasedBrainiac <Tobias.Turke@gmx.net>
2018-09-04 17:10:00 +0200

    excluded development test execution from module definition

---
+++ Scripts/TextFormatting/ExtractorMain.py
@@ -0,0 +1,35 @@
+# - *- coding: utf-8*-
+from TextFormatting.NodeStringExtractor import CheckOpenEnclosing, BuildCleanDefinedAMR
+open_par = '('
+closing_par = ')'
+content_example = """(c / cause-01
+      :ARG1 (l / live-01
+            :ARG0 (i / i
+                  :ARG0-of (t3 / talk-01 :polarity -
+                        :ARG2 (a5 / anyone)
+                        :ARG1-of (r / real-04)))
+            :ARG1 (l2 / life
+                  :poss i)
+            :manner (a / alone)
+            :duration (u / until
+                  :op1 (h / have-06
+                        :ARG0 i
+                        :ARG1 (a3 / accident
+                              :mod (p / plane))
+                        :location (d / desert :wiki "Sahara" :name (n / name :op1 "Desert" :op2 "of" :op3 "Sahara"))
+                        :time (b / before
+                              :op1 (n2 / now)
+                              :quant (t2 / temporal-quantity :quant 6
+                                    :unit (y / year)))))))"""
+
+
+print('Open-Close valid? [',CheckOpenEnclosing(content_example, '(', ')'),']')
+print(content_example)
+
+#pairs = GetParenthesisIndexPairs(content_example, open_par, closing_par)
+#for pair in pairs:
+#    print('[', content_example[pair[0]:pair[1]+1] ,'] in depth [',pair[2],']') 
+
+#print(GetNodes(content_example, content_example, 0, pairs, open_par, closing_par))
+
+print(BuildCleanDefinedAMR(content_example, open_par, closing_par))

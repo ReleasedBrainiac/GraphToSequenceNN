8299489881dc2a892ac3be7f35960245ef8e839e
ReleasedBrainiac <Tobias.Turke@gmx.net>
2018-09-05 16:16:43 +0200

    implemented 90% of the AMR String To Clean Graph Solution

--- Scripts/TextFormatting/NodeStringExtractor.py
+++ Scripts/TextFormatting/NodeStringExtractor.py
@@ -1,5 +1,35 @@
 # - *- coding: utf-8*-
-from TextFormatting.ContentSupport import isInStr, isNotInStr, isNotNone, isStr, isInt
+import re
+from TextFormatting.ContentSupport import isInStr, isNotInStr, isNotNone, isStr, isInt, toInt
+INDENTATION = 6
+
+def CountLeadingWhiteSpaces(raw_line):
+    if isStr(raw_line):
+        if isInStr(' ', raw_line):
+            return (len(raw_line) - len(raw_line.lstrip(' ')))
+        else:
+            return 0
+    else:
+        print('WRONG INPUT FOR [CountLeadingWhiteSpaces]')
+        return None
+
+def GetCurrentDepth(raw_line):
+    if isStr(raw_line):
+        if isInStr('', raw_line):
+            return toInt(CountLeadingWhiteSpaces(raw_line) / INDENTATION)
+        else:
+            return 0
+    else:
+        print('WRONG INPUT FOR [GetCurrentDepth]')
+        return None
+
+def CreateEncapsulatedDepthRefinedReplacement(raw_origin_line, replacement):
+    if isStr(raw_origin_line):
+        depth = GetCurrentDepth(raw_origin_line)
+        return ('\n'+AddLeadingSpace(replacement.lstrip(' '), (depth+1)))
+    else:
+        print('WRONG INPUT FOR [ReplacePolarity]')
+        return None
 
 def CountSubsStrInStr(content_str, search_element):
     if isStr(content_str) and isStr(search_element):
@@ -22,27 +52,86 @@ def CheckOpenEnclosing(content, open_par, closing_par):
         return None
 
 def GetEnclosedContent(content, open_par, closing_par):
-    if  isStr(content) and isStr(open_par) and isStr(closing_par) and isInStr(open_par, content) and isInStr(closing_par, content):
-
-        pos_open = content.index(open_par)
-        pos_close = content.rfind(closing_par)
-        return content[pos_open+1:pos_close]
+    if  isStr(content) and isStr(open_par) and isStr(closing_par):
+        if isInStr(open_par, content) and isInStr(closing_par, content):
+            pos_open = content.index(open_par)
+            pos_close = content.rfind(closing_par)
+            return content[pos_open+1:pos_close]
+        else:
+            return content
     else:
         print('WRONG INPUT FOR [GetEnclosedContent]')
         return None
 
+def EncloseSoloLabels(raw_line):
+    if isStr(raw_line):
+        if isInStr(':', raw_line):
+            ARGS_REGEX = '\B\:(?=\S*[+-]*)([a-zA-Z0-9*-]+)+( +[a-zA-Z]+)'
+            loot = re.findall(ARGS_REGEX, raw_line)
 
+            for loot_elem in loot:
+                joined_elem_regex = ''.join(loot_elem)
+                joined_elem_replace = ''.join([loot_elem[0], '('+loot_elem[1].lstrip(' ')+')'])
+                raw_line = re.sub(joined_elem_regex, joined_elem_replace, raw_line)      
+
+            return raw_line
+    else:
+        print('WRONG INPUT FOR [DeleteFlags]')
+        return None
+
+def DeleteFlags(raw_line):
+    if isStr(raw_line):
+        if isInStr(':', raw_line):
+
+            FLAG_REGEX = '\B\:(?=\S*[+-]*)([a-zA-Z0-9*-]+)+( \d)*'
+            return re.sub(FLAG_REGEX, '', raw_line)            
+        else:
+            return raw_line
+    else:
+        print('WRONG INPUT FOR [DeleteFlags]')
+        return None
+
+def ReplacePolarity(raw_line):
+    if isStr(raw_line):
+        if isInStr('-', raw_line):
+            POLARITY_SIGN_REGEX = '\s+\-\s*'
+            depth = GetCurrentDepth(raw_line)
+            replacement = AddLeadingSpace('(not)', (depth+1))
+            return re.sub(POLARITY_SIGN_REGEX, ('\n'+ replacement), raw_line)
+        else:
+            return raw_line   
+    else:
+        print('WRONG INPUT FOR [ReplacePolarity]')
+        return None
+
+def DeleteWordExtension(raw_line):
+    if isStr(raw_line):
+        if isInStr('-', raw_line):
+            EXTENSIO_REGEX = '\-\d+'
+            return re.sub(EXTENSIO_REGEX,'', raw_line)
+        else:
+            return raw_line
+    else:
+        print('WRONG INPUT FOR [DeleteWordExtension]')
+        return None
+    
 def ExploreAdditionalcontent(string_raw):
-    if isStr(string_raw) and isInStr(':', string_raw) and isInStr(' ', string_raw):
-        sub_line = string_raw[string_raw.find(':'):len(string_raw)]
-        
-        
+    if isStr(string_raw):
+        result = string_raw
+
+        if isInStr(':', result):
+            result = DeleteFlags(result)
+        if isInStr('-', result):
+            result = ReplacePolarity(result)
+            result = DeleteWordExtension(result)
+                
+        return result
     else:
         print('WRONG INPUT FOR [ExploreAdditionalcontent]')
         return None
 
 def GetUnformatedAMRString(raw_amr):
-    if isNotNone(raw_amr) and isStr(raw_amr):
+    if isStr(raw_amr):
         return ' '.join(raw_amr.split())
     else:
         print('WRONG INPUT FOR [GetUnformatedAMRString]')
@@ -50,16 +139,16 @@ def GetUnformatedAMRString(raw_amr):
 
 def AddLeadingSpace(str, amount):
     if(isStr(str)) and (isInt(amount)):
-        for _ in range(amount):
-            str = '      '+str
+        for _ in range((amount * INDENTATION)):
+            str = ' '+str
         return str
 
     else:
         print('WRONG INPUT FOR [AddLeadingSpace]')
         return None
 
-def RuleFormatting(amr_str, open_par, closing_par):
-    if isNotNone(amr_str) and isStr(amr_str):
+def NiceFormatting(amr_str, open_par, closing_par):
+    if isStr(amr_str) and isStr(open_par) and isStr(closing_par):
         depth = -1
         openings = amr_str.split(open_par)
         struct_contain = []
@@ -73,25 +162,23 @@ def RuleFormatting(amr_str, open_par, closing_par):
                 depth = depth - occourences
             
             if isInStr(':', new_line):
-                ExploreAdditionalcontent(new_line)
+                new_line = ExploreAdditionalcontent(new_line)
             
-            #print(new_line)
             struct_contain.append(new_line)
 
-        return None
+        returning = '\n'.join(struct_contain)
+        return returning
 
     else:
-        print('WRONG INPUT FOR [RuleFormatting]')
+        print('WRONG INPUT FOR [NiceFormatting]')
         return None
 
-
-def BuildCleanDefinedAMR(raw_amr, open_par, closing_par):
-    if isNotNone(raw_amr) and isStr(raw_amr):
-        amr_str = GetEnclosedContent(GetUnformatedAMRString(raw_amr), open_par, closing_par)
-        new_amr = RuleFormatting(amr_str, open_par, closing_par)
-        #return (open_par + new_amr + closing_par)
+def GenerateCleanAMR(raw_amr, open_par, closing_par):
+    if isStr(raw_amr) and isStr(open_par) and isStr(closing_par):
+        unformated_str = GetUnformatedAMRString(raw_amr)
+        node_enclosed_str = EncloseSoloLabels(unformated_str)
+        amr_str = GetEnclosedContent(node_enclosed_str, open_par, closing_par)
+        return NiceFormatting(amr_str, open_par, closing_par) 
     else:
-        print('WRONG INPUT FOR [BuildCleanDefinedAMR]')
+        print('WRONG INPUT FOR [GenerateCleanAMR]')
         return None
-
-# REGEX = \B\:(?=\S*[+-]*)([a-zA-Z*-]+)+

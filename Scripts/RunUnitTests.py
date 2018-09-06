bbd9d30b438ed5879e30074ce77d9bddc262b87e
ReleasedBrainiac <Tobias.Turke@gmx.net>
2018-09-06 19:13:46 +0200

    created file to start all tests @ once

---
+++ Scripts/RunUnitTests.py
@@ -0,0 +1,10 @@
+# - *- coding: utf-8*-
+from TestHandler.UnitTestingGraphHandler import RunGraphHandlerTests
+from TestHandler.UnitTestingNodeStringExtractor import RunNodeExtractorTests
+
+print('#### RunNodeExtractorTests ####')
+RunNodeExtractorTests()
+print()
+
+print('#### RunGraphHandlerTests ####')
+RunGraphHandlerTests()

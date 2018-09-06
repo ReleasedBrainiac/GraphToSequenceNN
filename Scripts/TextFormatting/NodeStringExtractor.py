818bf63bc5b6b4e627fea4c420fe6f35c0e2f013
ReleasedBrainiac <Tobias.Turke@gmx.net>
2018-09-06 13:25:20 +0200

    finalized graph reprocessing, its necessarry to implement tests

--- Scripts/TextFormatting/NodeStringExtractor.py
+++ Scripts/TextFormatting/NodeStringExtractor.py
@@ -1,7 +1,27 @@
 # - *- coding: utf-8*-
 import re
-from TextFormatting.ContentSupport import isInStr, isNotInStr, isNotNone, isStr, isInt, toInt
+from TextFormatting.ContentSupport import isInStr, isNotInStr, isNotNone, isStr, isInt, toInt, isNumber
+from TextFormatting.ContentSupport import hasContent, GetRandomInt
+
+
 INDENTATION = 6
+
+def CreateNewLabel(number):
+    inner_index = GetRandomInt(0, 50)
+    if isNumber(number) and (number > -1):
+        inner_index = number
+        
+    return 'Y' + str(inner_index) + 'Z'
+
+def CreateNewDefinedNode(label , content, open_par, closing_par):
+    if isStr(label) and isStr(open_par) and isStr(closing_par):
+        if isStr(content):
+            return open_par + label + ' / ' + content + closing_par
+        else:
+            return open_par + label + closing_par
+    else:
+        print('WRONG INPUT FOR [CreateNewDefinedNode]')
+        return None
 
 def CountLeadingWhiteSpaces(raw_line):
     if isStr(raw_line):
@@ -28,7 +48,7 @@ def CreateEncapsulatedDepthRefinedReplacement(raw_origin_line, replacement):
         depth = GetCurrentDepth(raw_origin_line)
         return ('\n'+AddLeadingSpace(replacement.lstrip(' '), (depth+1)))
     else:
-        print('WRONG INPUT FOR [ReplacePolarity]')
+        print('WRONG INPUT FOR [CreateEncapsulatedDepthRefinedReplacement]')
         return None
 
 def CountSubsStrInStr(content_str, search_element):
@@ -64,8 +84,9 @@ def GetEnclosedContent(content, open_par, closing_par):
         return None
 
 def EncloseSoloLabels(raw_line):
+    COLON = ':'
     if isStr(raw_line):
-        if isInStr(':', raw_line):
+        if isInStr(COLON, raw_line):
             ARGS_REGEX = '\B\:(?=\S*[+-]*)([a-zA-Z0-9*-]+)+( +[a-zA-Z]+)'
             loot = re.findall(ARGS_REGEX, raw_line)
 
@@ -76,7 +97,46 @@ def EncloseSoloLabels(raw_line):
 
             return raw_line
     else:
-        print('WRONG INPUT FOR [DeleteFlags]')
+        print('WRONG INPUT FOR [EncloseSoloLabels]')
+        return None
+
+def EncloseQualifiedStringInforamtions(raw_line, open_par, closing_par):
+    QUOTATION_MARK = '"'
+    SLASH = ' / '
+    QUALIFIED_STR_REGEX = '\B(\"\w+( \w+)*\")'
+    New_Nodes_Dict = {}
+
+    if isStr(raw_line):
+        if isInStr(QUOTATION_MARK, raw_line):
+            loot = re.findall(QUALIFIED_STR_REGEX, raw_line)
+            run_iter = -1
+
+            for loot_elem in loot:
+                found_elem = loot_elem[0]
+                label = None
+                content = None
+
+                if isNotNone(found_elem) and hasContent(found_elem) and found_elem.count(QUOTATION_MARK) == 2:
+                    if found_elem in New_Nodes_Dict:
+                        label = New_Nodes_Dict[found_elem]
+                        content = None
+
+                    else:     
+                        run_iter = run_iter + 1
+                        label = CreateNewLabel(run_iter)
+                        content = re.sub('"','',found_elem)
+                        New_Nodes_Dict[found_elem] = label
+                    
+                    replace = CreateNewDefinedNode(label, content, open_par, closing_par)
+                    raw_line = raw_line.replace(found_elem, replace, 1)
+
+                else:
+                    print('WRONG DEFINITION ['+ found_elem + ']FOUND IN INPUT FOR [EncloseQualifiedStringInforamtions]')
+                    continue   
+
+            return raw_line
+    else:
+        print('WRONG INPUT FOR [EncloseQualifiedStringInforamtions]')
         return None
 
 def DeleteFlags(raw_line):
@@ -177,7 +237,8 @@ def GenerateCleanAMR(raw_amr, open_par, closing_par):
     if isStr(raw_amr) and isStr(open_par) and isStr(closing_par):
         unformated_str = GetUnformatedAMRString(raw_amr)
         node_enclosed_str = EncloseSoloLabels(unformated_str)
-        amr_str = GetEnclosedContent(node_enclosed_str, open_par, closing_par)
+        name_enclosed_str = EncloseQualifiedStringInforamtions(node_enclosed_str, open_par, closing_par)
+        amr_str = GetEnclosedContent(name_enclosed_str, open_par, closing_par)
         return NiceFormatting(amr_str, open_par, closing_par) 
     else:
         print('WRONG INPUT FOR [GenerateCleanAMR]')

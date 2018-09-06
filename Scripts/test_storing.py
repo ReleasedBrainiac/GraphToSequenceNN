bdc81c4f52862f9b9c936fcd303d5bf8337a4bf0
ReleasedBrainiac <Tobias.Turke@gmx.net>
2018-09-06 17:51:56 +0200

    set flag for dev test

--- Scripts/test_storing.py
+++ Scripts/test_storing.py
@@ -6,7 +6,7 @@ from TextFormatting.DatasetProvider import SavePipeline as Pipe
 max_length = -1
 output_extender = 'dc.ouput'
 inpath = '../Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-dev.txt'
-save_as_arm = False
+save_as_arm = True
 print_activated_anynode = False  # this wont work if you only store arm cause no anynode is calced!
 
 Pipe(inpath, output_extender, max_length, save_as_arm, print_activated_anynode)

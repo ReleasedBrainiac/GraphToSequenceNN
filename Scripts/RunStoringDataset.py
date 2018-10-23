5d58506f1a34baa03710ed1bbec00eefe12a2f50
Tobias Turke <Tobias.Turke@comparex.de>
2018-10-23 16:35:05 +0200

    changed path

--- Scripts/RunStoringDataset.py
+++ Scripts/RunStoringDataset.py
@@ -5,7 +5,8 @@ from DatasetHandler.DatasetProvider import DatasetPipelines as Pipe
 #===========================================================#
 max_length = -1
 output_extender = 'dc.ouput'
-inpath = '../Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-dev.txt'
+#inpath = '../Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-dev.txt'
+inpath = '../Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-training.txt'
 save_as_arm = True
 print_activated_anynode = False  # this wont work if you only store arm cause no anynode is calced!
 

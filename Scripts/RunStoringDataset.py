76dad9aef20e81cf65401177e243ad8870017108
Tobias Turke <Tobias.Turke@comparex.de>
2018-11-06 20:42:52 +0100

    changed values

--- Scripts/RunStoringDataset.py
+++ Scripts/RunStoringDataset.py
@@ -7,8 +7,10 @@ max_length = -1
 output_extender = '.ouput'
 #inpath = '../Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-dev.txt'
 inpath = './Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-training.txt'
-save_amr = False
+
+as_amr = True
+saving = True
 print_activated = True  # this wont work if you only store arm cause no anynode is calced!
 
-pipe = DatasetPipelines(in_path=inpath, output_path_extender=output_extender, max_length=max_length, show_feedback=print_activated, as_amr=save_amr)
-pipe.SaveData()
+pipe = DatasetPipelines(in_path=inpath, output_path_extender=output_extender, max_length=max_length, show_feedback=print_activated, saving=saving)
+pipe.SaveData(as_amr=as_amr)

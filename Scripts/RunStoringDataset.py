c71e85fa5da839a99227454180eba0998e62558f
Tobias Turke <Tobias.Turke@comparex.de>
2018-11-06 19:49:28 +0100

    redo class call depending on restructuring the DatasetProvider class

--- Scripts/RunStoringDataset.py
+++ Scripts/RunStoringDataset.py
@@ -1,4 +1,4 @@
-from DatasetHandler.DatasetProvider import DatasetPipelines as Pipe
+from DatasetHandler.DatasetProvider import DatasetPipelines
 
 #===========================================================#
 #                            Test                           #
@@ -7,7 +7,8 @@ max_length = -1
 output_extender = '.ouput'
 #inpath = '../Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-dev.txt'
 inpath = './Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-training.txt'
-save_as_arm = True
-print_activated_anynode = False  # this wont work if you only store arm cause no anynode is calced!
+save_amr = False
+print_activated = True  # this wont work if you only store arm cause no anynode is calced!
 
-Pipe().SavePipeline(inpath, output_extender, max_length, save_as_arm, print_activated_anynode)
+pipe = DatasetPipelines(in_path=inpath, output_path_extender=output_extender, max_length=max_length, show_feedback=print_activated, as_amr=save_amr)
+pipe.SaveData()

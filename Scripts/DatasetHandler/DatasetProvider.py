b5abe1950a833e999a41de60544de4695e29e165
Tobias Turke <Tobias.Turke@comparex.de>
2018-11-06 20:45:53 +0100

    remove unecessary params and removed various redundant calls

--- Scripts/DatasetHandler/DatasetProvider.py
+++ Scripts/DatasetHandler/DatasetProvider.py
@@ -32,36 +32,66 @@ class DatasetPipelines:
     gt_converter = GTConverter()
     t_parser = TParser()
     g_parser = None
-    keep_edges = False
+
+    # Path content
+    in_path = None
+    out_path_extender = '.ouput'
+    context_max_length = -1
+
+    # Switches
+    is_keeping_edges = False
+    is_saving = False
+    is_showing_feedback = False
+    as_amr = False
+
+    def __init__(self, in_path=None, output_path_extender=None, max_length=-1, saving=False,show_feedback=False, keep_edges=False):
+        try:
+            self.in_path = setOrDefault(in_path, self.constants.TYP_ERROR, isStr(in_path))
+
+            if isStr(output_path_extender): self.out_path_extender = output_path_extender
+
+            self.context_max_length = setOrDefault(max_length, -1, isInt(max_length))
+
+            if isBool(show_feedback): self.is_showing_feedback = show_feedback
+
+            if isBool(saving): self.is_saving = saving
+
+            if isBool(keep_edges): self.is_keeping_edges = keep_edges
+
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [DatasetProvider.__init__]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
     def RemoveEnclosingAngleBracket(self, in_sentence):
         """
         This method clean up the a given amr extracted sentence from text formating markup.
             :param in_sentence: raw sentence AMR split element 
         """
-        if(isStr(in_sentence)):
+        try:
             in_sentence = re.sub('<[^/>][^>]*>','', in_sentence)
             in_sentence = re.sub('</[^>]+>','', in_sentence)
             in_sentence = re.sub('<[^/>]+/>','', '#'+in_sentence)
             return in_sentence+'\n'
-        else:
-            print('WRONG INPUT FOR [RemoveEnclosingAngleBracket]')
-            return None
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [DatasetProvider.RemoveEnclosingAngleBracket]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
-    def ReforgeSemanticToCleanMinimalARM(self, semantic, sem_flag):
+    def CleanReforgeAmrSemantic(self, semantic, semantic_flag):
         """
         This function allows to convert a raw AMR semantic (graph-) string  
         into a cleaned and minimalized version of it.
             :param semantic: raw semantic input
-            :param sem_flag: marker/delim to add to cleaned semantic
+            :param semantic_flag: marker/delim to add to cleaned semantic
         """
-        if isStr(semantic) and isStr(sem_flag):
+        try:
             store = semantic
             cleaner = Cleaner(open_bracket=self.parenthesis[0], 
                               close_bracket=self.parenthesis[1], 
                               input_context=semantic,
                               input_extension_dict=self.extension_dict,
-                              keep_edges = self.keep_edges
+                              keep_edges = self.is_keeping_edges
                               )
 
             self.extension_dict = cleaner.extension_dict
@@ -71,176 +101,148 @@ class DatasetPipelines:
             if isNone(semantic):
                 print(store)
 
-            half_cleaned_sem = '#'+sem_flag+' \n'+semantic+'\n'
+            half_cleaned_sem = '#'+semantic_flag+' \n'+semantic+'\n'
             out = half_cleaned_sem+'\n'
             return out
-        else:
-            print('WRONG INPUT FOR [ReforgeSemanticToCleanMinimalARM]')
-            return None
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [DatasetProvider.CleanReforgeAmrSemantic]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
-    def ReforgeSemanticToMinimalAnyTree(self, semantic, sem_flag, print_console, to_process):
+    def CleanReforgeTreeSemantic(self, semantic, semantic_flag):
         """
         This function allows to convert a raw AMR semantic (graph-) string
         into a cleaned and minimalized anytree reprenstation of it.
             :param semantic: raw semantic input
-            :param sem_flag: marker/delim to add to cleaned semantic
-            :param print_console: If True you get console output for GraphSalvage.Execute
-            :param to_process: If False you get a JsonString for saving in file.
+            :param semantic_flag: marker/delim to add to cleaned semantic
         """
-        if(isStr(semantic)) and (isStr(sem_flag)) and (isBool(print_console)) and (isBool(to_process)):
-            half_cleaned_sem = '#'+sem_flag+' '+semantic+'\n'
+        try:
+            half_cleaned_sem = '#'+semantic_flag+' '+semantic+'\n'
             out = half_cleaned_sem+'\n'
-            return self.t_parser.Execute(out, sem_flag, print_console, to_process)
+            return self.t_parser.Execute(out, semantic_flag, self.is_showing_feedback, self.is_saving)
+            #//TODO remove or use!
             #self.g_parser = GParser(out, self.extension_dict)
             #return self.g_parser.GetNetworkxGraph()
-        else:
-            print('WRONG INPUT FOR [ReforgeSemanticToMinimalAnyTree]')
-            return None
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [DatasetProvider.CleanReforgeTreeSemantic]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
-    def GetSingleDatasetPair(self, sent_flag, sem_flag, sentence, semantic, want_as_arm, isShowConsole, isNotStoring):
+    def CollectDatasetPair(self, sentence_flag, semantic_flag, sentence, semantic):
         """
         This function collect a data pair from raw sentences and semantics.
-        ATTENTION: [want_as_arm = true] does not support conversion with ConvertToTensorMatrices!
-            :param sent_flag: a marker to attach to the cleaned sentence, make it easier to find later
-            :param sem_flag: a marker to attach to the cleaned semantic, make it easier to find later
+        ATTENTION: [as_amr = true] does not support conversion with ConvertToTensorMatrices!
+            :param sentence_flag: a marker to attach to the cleaned sentence, make it easier to find later
+            :param semantic_flag: a marker to attach to the cleaned semantic, make it easier to find later
             :param sentence: raw input of the AMR sentence
             :param semantic: raw input of the AMR semantic
-            :param want_as_arm: True then it return a tree-like formated AMR string for the semantic entry
-            :param isShowConsole: True then it show in and out of the GraphSalvage.Cleaner on console
-            :param isNotStoring: True you get GraphTree as AnyTree for further usage, else you get a JsonString for saving in file
         """
-        if (isStr(sent_flag)) and (isStr(sem_flag)) and (isStr(sentence)) and (isStr(semantic)) and (isBool(want_as_arm)) and (isBool(isShowConsole)) and (isBool(isNotStoring)):
-            sentence = sent_flag+' '+sentence
+        try:
+            #//TODO here check a semantic is okay with Cleaner.isCleaned => if not discard 
+            sentence = sentence_flag+' '+sentence
             sentence = self.RemoveEnclosingAngleBracket(sentence)
-            if(want_as_arm):
-                semantic = self.ReforgeSemanticToCleanMinimalARM(semantic, sem_flag)
+            if(self.as_amr):
+                semantic = self.CleanReforgeAmrSemantic(semantic, semantic_flag)
                 return [sentence, semantic]
             else:
-                semantic = self.ReforgeSemanticToMinimalAnyTree(semantic, sem_flag, isShowConsole, isNotStoring)
+                semantic = self.CleanReforgeTreeSemantic(semantic, semantic_flag)
                 return [sentence, semantic]
-        else:
-            print('WRONG INPUT FOR [GetSingleDatasetPair]')
-            return [None, None]
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [DatasetProvider.CollectDatasetPair]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
-    def GetMultiDatasetPairs(self, sent_flag, sem_flag, sent_array, sem_array, want_as_arm, isShowConsole, isNotStoring):
+    def CollectAllDatasetPairs(self, sentence_flag, semantic_flag, sent_array, sem_array):
         """
         This function collect multiples pairs of semantic and sentence data as list of data pairs.
         For this case we pass arrays of raw sentences and semantics, 
         where index i in both arrays point to a sentence and the corresponding semantic.
-            :param sent_flag: a marker to attach to the cleaned sentence, make it easier to find later
-            :param sem_flag: a marker to attach to the cleaned semantic, make it easier to find later
+            :param sentence_flag: a marker to attach to the cleaned sentence, make it easier to find later
+            :param semantic_flag: a marker to attach to the cleaned semantic, make it easier to find later
             :param sent_array: Array of the raw input of the AMR sentences
             :param sem_array: Array of the raw input of the AMR semantics
-            :param want_as_arm: True then it return a tree-like formated AMR string for the semantic entry
-            :param isShowConsole: True then it show in and out of the GraphSalvage.Cleaner on console
-            :param isNotStoring: True you get GraphTree as AnyTree for further usage, else you get a JsonString for saving in file
         """
-        if (isStr(sent_flag)) and (isStr(sem_flag)) and (isList(sent_array)) and (isList(sem_array)) and (isBool(want_as_arm)) and (isBool(isShowConsole)) and (isBool(isNotStoring)):
+        try:
             dataset_pairs_sent_sem = []
             for i in range(min(len(sent_array), len(sem_array))):
-                dataset_pairs_sent_sem.append(self.GetSingleDatasetPair(sent_flag, sem_flag, sent_array[i], sem_array[i], want_as_arm, isShowConsole, isNotStoring))
+                dataset_pairs_sent_sem.append(self.CollectDatasetPair(sentence_flag, semantic_flag, sent_array[i], sem_array[i]))
 
             return dataset_pairs_sent_sem
-        else:
-            print('WRONG INPUT FOR [GetMultiDatasetPair]')
-            return []
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [DatasetProvider.CollectAllDatasetPairs]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
-    def BasicPipeline(self, inpath, output_extender, max_length, save_as_arm, print_console, is_not_saving):
+    def Pipeline(self):
         """
         This function collect the cleaned sentence graphs as:
-        1. AMR string representation [save_as_arm = True]
+        1. AMR string representation [as_amr_structure = True]
         2. AnyTree [else]:
             1. as JSON     [is_not_saving = true]
             2. as AnyNode  [else]
-
-            :param inpath: path of dataset text file
-            :param output_extender: extender to define result filename
-            :param max_length: max allows length for sentences
-            :param save_as_arm: output will be save as tree like formated AMR string
-            :param print_console: show all reforging at the Cleaner on console
-            :param is_not_saving: set result to JSON or AnyNode [save_as_arm = False]
         """
-        semantics  = []
-        sentences  = []
-        sents_lens = []
-        sema_lens  = []
+        try:
 
-        max_length = setOrDefault(max_length, -1, isInt(max_length))
-        inpath  = setOrDefault(inpath, self.constants.TYP_ERROR, isStr(inpath))
+            dataset = Reader(self.in_path).GroupReadAMR()
+            dataset=dataset[1:len(dataset)]
 
-        dataset = Reader(inpath).GroupReadAMR()
-        dataset=dataset[1:len(dataset)]
-        
-        sents_lens, sema_lens, sentences, semantics = self.extractor.Extract(dataset, 
-                                                                             max_length, 
-                                                                             self.constants.SENTENCE_DELIM, 
-                                                                             self.constants.FILE_DELIM)
+            extracted = self.extractor.Extract(dataset, 
+                                               self.context_max_length, 
+                                               self.constants.SENTENCE_DELIM, 
+                                               self.constants.FILE_DELIM)        
 
-        mw_value_sen = self.eval_Helper.CalculateMeanValue(sents_lens)
-        mw_value_sem = self.eval_Helper.CalculateMeanValue(sema_lens)     
+            mean_value_sentences = self.eval_Helper.CalculateMeanValue(extracted[0])
+            mean_value_semantics = self.eval_Helper.CalculateMeanValue(extracted[1])     
 
-        data_pairs = self.GetMultiDatasetPairs(self.constants.SENTENCE_DELIM, 
-                                               self.constants.SEMANTIC_DELIM, 
-                                               sentences, 
-                                               semantics, 
-                                               save_as_arm, 
-                                               print_console, 
-                                               is_not_saving)
+            data_pairs = self.CollectAllDatasetPairs(self.constants.SENTENCE_DELIM, 
+                                                     self.constants.SEMANTIC_DELIM, 
+                                                     extracted[2], 
+                                                     extracted[3])
 
-        print('Max restriction: ', max_length)
-        print('Path input: ', inpath)
-        print('Count sentences: ', len(sentences))
-        print('Count semantics: ', len(semantics))
-        print('Mean sentences: ', mw_value_sen)
-        print('Mean semantics: ', mw_value_sem)
-        print('Look_up: \n', self.extension_dict)
+            print('Max restriction: ', self.context_max_length)
+            print('Path input: ', self.in_path)
+            print('Count sentences: ', len(extracted[2]))
+            print('Count semantics: ', len(extracted[3]))
+            print('Mean sentences: ', mean_value_sentences)
+            print('Mean semantics: ', mean_value_semantics)
+            print('AMR? ', self.as_amr)
+            print('Look_up: \n', self.extension_dict)
 
-        return [mw_value_sen, mw_value_sem, max_length, data_pairs]
+            return data_pairs
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [DatasetProvider.Pipeline]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
-    def SavePipeline(self, inpath, output_extender, max_length, save_as_arm, print_console):
+    def SaveData(self, as_amr):
         """
-        This function calls the BasicPipeline and store the desired results.
-            :param inpath: path of dataset text file
-            :param output_extender: extender to define result filename
-            :param max_length: max allows length for sentences
-            :param save_as_arm: output will be save as tree like formated AMR string
-            :param print_console: show all reforging at the Cleaner on console
+        This function calls the Pipeline and store the desired results.
+            :param as_amr: if True save as amr string otherwise as anytree json string
         """
-        _, _, _, data_pairs = self.BasicPipeline(inpath, 
-                                                 output_extender, 
-                                                 max_length, 
-                                                 save_as_arm, 
-                                                 print_console, 
-                                                 False)
-        
-        writer = Writer(inpath, output_extender, data_pairs)
-        outpath = writer.GetOutputPath()
-        writer.StoreAMR()
+        try:
+            
+            if isBool(as_amr): self.as_amr = as_amr
+            writer = Writer(self.in_path, 
+                            self.out_path_extender, 
+                            self.Pipeline())
+            outpath = writer.GetOutputPath()
+            #writer.StoreAMR()
 
-        if (print_console):
-            print('outpath: ', outpath)
-        
-        return None
-        
-
-    def DataPipeline(self, inpath, output_extender, max_length, save_as_arm, print_console):
-        """
-        This function calls the BasicPipeline and return the cleaned dataset for ANN usage.
-            :param inpath: path of dataset text file
-            :param output_extender: extender to define result filename
-            :param max_length: max allows length for sentences
-            :param save_as_arm: output will be save as tree like formated AMR string
-            :param print_console: show all reforging at the Cleaner on console
-        """
-        if(save_as_arm == True):
-            print('Processing dataset on AMR string representation not supported! Please set [save_as_arm=FALSE]!')
+            if (self.is_showing_feedback): print('outpath: ', outpath)
+            
             return None
-        else:
-            data_pairs = self.BasicPipeline(inpath, 
-                                            output_extender, 
-                                            max_length, 
-                                            save_as_arm, 
-                                            print_console, 
-                                            True)[3]
-
-            return self.gt_converter.GetDataSet(data_pairs)
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [DatasetProvider.SaveData]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+        
+    def ProvideData(self):
+        """
+        This function calls the Pipeline and return the cleaned dataset for ANN usage.
+        """
+        try:
+            return self.gt_converter.GetDataSet(self.Pipeline())
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [DatasetProvider.ProvideData]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)

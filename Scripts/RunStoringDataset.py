5e40c6ebee85032172ebc24544c07172b929ac00
Tobias Turke <Tobias.Turke@comparex.de>
2018-11-15 12:20:55 +0100

    changed call of the function

--- Scripts/RunStoringDataset.py
+++ Scripts/RunStoringDataset.py
@@ -4,12 +4,13 @@ from DatasetHandler.DatasetProvider import DatasetPipelines
 #                            Test                           #
 #===========================================================#
 #inpath = '../Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-dev.txt'
-inpath = './Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-training.txt'
+#inpath = './Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-training.txt'
+inpath = './Datasets/Raw/AMR Bio/amr-release-dev-bio.txt'
 
 # show_feedback = True => this wont work if you only store arm cause no anynode is calced!
 
 pipe = DatasetPipelines(in_path=inpath, 
-                        output_path_extender='.ouput', 
+                        output_path_extender='ouput', 
                         max_length=-1, 
                         show_feedback=True, 
                         saving=True, 

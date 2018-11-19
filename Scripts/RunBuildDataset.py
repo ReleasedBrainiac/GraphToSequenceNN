fad381a44cf2e7d909abc8182d7ff0d84cc806b4
Tobias Turke <Tobias.Turke@comparex.de>
2018-11-19 11:26:14 +0100

    changed calling structure

--- Scripts/RunBuildDataset.py
+++ Scripts/RunBuildDataset.py
@@ -7,7 +7,7 @@ inpath = './Datasets/Raw/Der Kleine Prinz AMR/amr-bank-struct-v1.6-training.txt'
 pipe = DatasetPipeline(in_path=inpath, 
                        output_path_extender='dc.ouput', 
                        max_length=-1, 
-                       show_feedback=True, 
+                       show_feedback=False, 
                        saving=False, 
                        keep_edges=False
                        )

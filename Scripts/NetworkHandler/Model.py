bad7ca431a78e2c68ae7ed8f46f8791010924386
Tobias Turke <Tobias.Turke@comparex.de>
2019-01-15 16:01:37 +0100

    added inital file for the network model.

---
+++ Scripts/NetworkHandler/Model.py
@@ -0,0 +1,12 @@
+
+'''
+    This class is based on "Graph2Seq: Graph to Sequence Learning with Attention-based Neural Networks" by Kun Xu et al.  
+    The paper can be found at: https://arxiv.org/abs/1804.00823
+    A implementation example of this paper from the IBM research team can be found at: https://github.com/IBM/Graph2Seq 
+'''
+
+class GraphToSequenceModel:
+
+    # TODO missing implementation
+    def __init__(self, *args, **kwargs):
+        return super().__init__(*args, **kwargs)

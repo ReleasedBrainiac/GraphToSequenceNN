13abff4facdb0f7cd6ee5ff306f78490cefceb93
Tobias Turke <Tobias.Turke@comparex.de>
2019-01-21 12:51:59 +0100

    added new file to try my own way of implementation... i stuck a little with the un commended version of ibm... also with the minimum example cause its based on a text to only integer abstraction

---
+++ Scripts/NetworkHandler/Model2.py
@@ -0,0 +1,9 @@
+'''
+    This class is based on "Graph2Seq: Graph to Sequence Learning with Attention-based Neural Networks" by Kun Xu et al.  
+    The paper can be found at: https://arxiv.org/abs/1804.00823
+    A implementation example of this paper from the IBM research team can be found at: https://github.com/IBM/Graph2Seq 
+
+    This model implementation is my own interpretation of the provided structure in tht paper.
+'''
+
+class Graph2SeqNN(object):

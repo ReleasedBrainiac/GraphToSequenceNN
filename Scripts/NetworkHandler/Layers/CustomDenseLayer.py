a5151bbb30f19d02be7acced6b4e27925a9570fc
Tobias Turke <Tobias.Turke@comparex.de>
2019-01-21 16:11:27 +0100

    testing structure changes to convert tensorflow workflow into keras api definition

--- Scripts/NetworkHandler/Layers/CustomDenseLayer.py
+++ Scripts/NetworkHandler/Layers/CustomDenseLayer.py
@@ -1,49 +1,94 @@
 import tensorflow as tf
+import keras
+from keras import backend as K
+from keras import initializations, activations
+from keras.layers import Layer
+
 from inits import zeros
 import configure as conf
-from BasicLayer import Layer
+#from BasicLayer import Layer
 
 '''
     This class is based on "Graph2Seq: Graph to Sequence Learning with Attention-based Neural Networks" by Kun Xu et al.  
     The paper can be found at: https://arxiv.org/abs/1804.00823
     The original implementation snipped from the IBM research team can be found at: https://github.com/IBM/Graph2Seq/blob/master/main/layers.py
 
+    Extracted the implementation strategy of the IBM example for Keras API users.
     Some smaller changes may depend on the structure of my data or my initial network implementation strategy.
+
+    Basics for CustomLayer resource: https://keras.io/layers/writing-your-own-keras-layers/
 '''
 
 
 #TODO try catch and documentation
 class Dense(Layer):
 
-    def __init__(self, input_dim, 
+    def __init__(self, 
+                 input_dim, 
                  output_dim, 
+                 name='kernel_weights',
+                 weight_init='glorot_uniform',
                  dropout=0.,
-                 act=tf.nn.relu, 
+                 activation=activations.relu, 
                  placeholders=None, 
                  bias=True, 
-                featureless=False,
-                 sparse_inputs=False, **kwargs):
+                 bias_init='zeros',
+                 featureless=False,
+                 sparse_inputs=False, 
+                 **kwargs):
+
+
         super(Dense, self).__init__(**kwargs)
 
-        self.dropout = dropout
-        self.act = act
-        self.featureless = featureless
-        self.bias = bias
+        
         self.input_dim = input_dim
         self.output_dim = output_dim
+        self.name = name
+        self.kernel_initializers = initializations.get(weight_init)
+        self.bias = bias
+        self.bias_initializers = initializations.get(bias_init)
+        self.dropout = dropout
+        self.activation = activation
+        self.featureless = featureless
+        
+        
 
         # helper variable for sparse dropout
         self.sparse_inputs = sparse_inputs
-        if sparse_inputs:
-            self.num_features_nonzero = placeholders['num_features_nonzero']
+        if sparse_inputs: self.num_features_nonzero = placeholders['num_features_nonzero']
 
         with tf.variable_scope(self.name + '_vars'):
-            self.vars['weights'] = tf.get_variable('weights', shape=(input_dim, output_dim),
-                                         dtype=tf.float32,
-                                         initializer=tf.contrib.layers.xavier_initializer(),
-                                         regularizer=tf.contrib.layers.l2_regularizer(conf.weight_decay))
-            if self.bias:
-                self.vars['bias'] = zeros([output_dim], name='bias')
+            self.vars['weights'] = tf.get_variable( 'weights', 
+                                                    shape=(input_dim, output_dim),
+                                                    dtype=tf.float32,
+                                                    initializer=tf.contrib.layers.xavier_initializer(),
+                                                    regularizer=tf.contrib.layers.l2_regularizer(conf.weight_decay))
+
+            if self.bias: self.vars['bias'] = zeros([output_dim], name='bias')
+
+
+    # Here we build the weight matrix
+    def build(self, input_shape):
+        self.kernel = self.add_weight(name=self.name, 
+                                      shape=(self.input_dim, self.output_dim),
+                                      initializer=keras.initializers.glorot_normal(seed=None),
+                                      trainable=True)
+        super(Dense, self).build(input_shape)
+
+
+    # Here lives the layer logic part
+    def call(self, x):
+        return K.dot(x, self.kernel)
+
+    # Here lives the output shape transformation logic
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.output_dim)
+
+
+
+
+
+
 
     def _call(self, inputs):
         x = inputs
@@ -56,4 +101,4 @@ class Dense(Layer):
         # bias
         if self.bias: output += self.vars['bias']
 
-        return self.act(output)
+        return self.activation(output)

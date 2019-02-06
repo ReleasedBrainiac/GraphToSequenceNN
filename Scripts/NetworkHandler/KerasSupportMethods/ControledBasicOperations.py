1cc029c0e4bc02ba355d587c0f711daa5c39278b
Tobias Turke <Tobias.Turke@comparex.de>
2019-02-06 18:20:02 +0100

    added documentation and reference

--- Scripts/NetworkHandler/KerasSupportMethods/ControledBasicOperations.py
+++ Scripts/NetworkHandler/KerasSupportMethods/ControledBasicOperations.py
@@ -1,20 +1,45 @@
 from keras import backend as K
 from NetworkHandler.KerasSupportMethods import AssertIsTensor, AssertTensorDotDim, AssertAddTensorToTensor
 
+'''
+    This class is based on https://keras.io/backend/#backend-functions.
+    This implementation is refined for Keras usage.
+'''
+
 class ControledTensorOperations():
-    def ControledConcatenation(prev_features, new_features, axis=1):
-        AssertIsTensor(prev_features)
-        AssertIsTensor(new_features)
-        return K.concatenate([prev_features, new_features], axis)
+    """
+    This class allow to perform various basic Keras operations in addition with some control statements!
+    """
 
-    def ControledWeightDotProduct(aggregated_vectors, weights):
-        AssertIsTensor(aggregated_vectors)
-        AssertIsTensor(weights)
-        AssertTensorDotDim(aggregated_vectors, weights)
-        return K.dot(aggregated_vectors, weights)
+    def ControledConcatenation(initial_tensor, concat_tensor, axis=1):
+        """
+        This function perform a concatenation of 2 tensors.
+            :param initial_tensor: initial tensor
+            :param concat_tensor: tensor to be concatenated to the initial tensor
+            :param axis: concatenation axis (default = 1)
+        """
+        AssertIsTensor(initial_tensor)
+        AssertIsTensor(concat_tensor)
+        return K.concatenate([initial_tensor, concat_tensor], axis)
 
-    def ControledBiased(weighted, bias):
-        AssertIsTensor(weighted)
+    def ControledWeightDotProduct(matrix_left, matrix_top):
+        """
+        This function perform the algebraic tensor matrix product between matrix_left and matrix_top.
+            :param matrix_left: the first tensor matrix
+            :param matrix_top: the second tensor matrix
+        """
+        AssertIsTensor(matrix_left)
+        AssertIsTensor(matrix_top)
+        AssertTensorDotDim(matrix_left, matrix_top)
+        return K.dot(matrix_left, matrix_top)
+
+    def ControledBiased(tensor, bias):
+        """
+        This function add a bias to a given tensor matrix.
+            :param tensor: the tensor matrix
+            :param bias: the desired bias
+        """   
+        AssertIsTensor(tensor)
         AssertIsTensor(bias)
-        AssertAddTensorToTensor(weighted, bias) 
-        return weighted + bias
+        AssertAddTensorToTensor(tensor, bias) 
+        return tensor + bias

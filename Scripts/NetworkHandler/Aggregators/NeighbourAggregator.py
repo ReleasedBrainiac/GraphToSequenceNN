0378f01484dfe99c37de72919e6605017f6d40cd
Tobias Turke <Tobias.Turke@comparex.de>
2019-02-06 17:36:20 +0100

    moved controled methods to seperate file

--- Scripts/NetworkHandler/Aggregators/NeighbourAggregator.py
+++ Scripts/NetworkHandler/Aggregators/NeighbourAggregator.py
@@ -1,5 +1,5 @@
 from NetworkHandler.KerasSupportMethods import KerasEval as KE
-from NetworkHandler.KerasSupportMethods import AssertTensorShapeEqual, AssertIsTensor, AssertTensorDotDim, AssertVectorLength
+from NetworkHandler.KerasSupportMethods import AssertTensorShapeEqual, AssertIsTensor, AssertTensorDotDim, AssertVectorLength, AssertAddTensorToTensor
 from keras import backend as K
 
 #TODO missing docu
@@ -7,28 +7,28 @@ from keras import backend as K
 
 class NeighbourAggregator():
 
-    def __init__(self, features, neighbouring, aggregator='mean',**kwargs):
+    def __init__(self, features, neighbouring, axis=1, aggregator='mean',**kwargs):
         self.initial_features = features
         self.new_features = None
         self.neighbouring = neighbouring
         self.aggregator = aggregator
         self.zero_vec = K.zeros(features[0,:].shape, dtype='float32')
-
+        self.axis = axis
 
     def AssertNeighbourMatch(self, found_neighbours, neighbouring_look_up):
         check = len(found_neighbours) == KE(K.sum(neighbouring_look_up))
         assert (check), ('Amount of existing neigbours did not match the found neigbours count!')
         return check
 
-    def MaxPoolAggregator(self, vectors, axis=0):
+    def MaxPoolAggregator(self, vectors, axis=1):
         AssertIsTensor(vectors)
         return K.max(vectors, axis)
 
-    def MeanAggregator(self, vectors, axis=0):
+    def MeanAggregator(self, vectors, axis=1):
         AssertIsTensor(vectors)
         return K.mean(vectors, axis)
 
-    def PerformAggregator(self, found_vectors, features_shape, axis=0, aggregator='mean'):
+    def PerformAggregator(self, found_vectors, features_shape, axis=1, aggregator='mean'):
         aggregated = None
         
         if (aggregator=='mean'):
@@ -38,7 +38,6 @@ class NeighbourAggregator():
                 
         AssertTensorShapeEqual(aggregated, features_shape)
         return aggregated
-
 
     def GetSingleVectorFeatures(self, max_neighbours, vertex_neighbours, last_features):
         found_neigbours = []
@@ -53,7 +52,7 @@ class NeighbourAggregator():
         return found_neigbours
 
 
-    def GetAllVectorsFeatures(self, iterator, last_features, in_neighbouring, axis=0, aggregator='mean'):
+    def GetAllVectorsFeatures(self, iterator, last_features, in_neighbouring, axis=1, aggregator='mean'):
         updated_features = []
         for i in range(iterator):
             vertex_neighbours = in_neighbouring[i,:]
@@ -61,9 +60,10 @@ class NeighbourAggregator():
             found_neigbours = self.GetSingleVectorFeatures(max_neighbours, vertex_neighbours, last_features)
             
             if len(found_neigbours) < 1: found_neigbours.append(self.zero_vec)
-                
+            
+            #TODO hier muss der max pool oder mean hin da hier dir neig_vecs gefunden und bearbeitet werden
             found_vectors = K.variable(value=found_neigbours, dtype='float32')
-            updated_features.append(self.PerformAggregator(found_vectors, last_features[0,:], 0, aggregator))
+            updated_features.append(self.PerformAggregator(found_vectors, last_features[0,:], axis, aggregator))
             
             
         final_features = K.stack(updated_features)
@@ -78,7 +78,7 @@ class NeighbourAggregator():
         self.new_features = self.GetAllVectorsFeatures( self.neighbouring.shape[0],
                                                         self.initial_features, 
                                                         self.neighbouring,
-                                                        0,
+                                                        self.axis,
                                                         self.aggregator)
 
     def OverwriteNewFeatures(self, new_features):
@@ -87,18 +87,8 @@ class NeighbourAggregator():
         self.new_features = new_features
         AssertIsTensor(self.new_features)
 
-    def ControledConcatenation(self, axis=1):
-        AssertIsTensor(self.initial_features)
-        AssertIsTensor(self.new_features)
-        AssertVectorLength(self.initial_features[0,:], self.new_features[0,:])
-        return K.concatenate([self.initial_features, self.new_features], axis)
+    def PerformMaxPoolLayersCall(self, mp_layers, neigh_vecs, batch_size, max_neighbours):
+        h_reshaped = K.reshape(neigh_vecs, (batch_size * max_neighbours, self.neigh_input_dim))
+        for l in self.mp_layers: h_reshaped = l(h_reshaped)
 
-    def ControledAdd(self):
-        AssertIsTensor(self.initial_features)
-        AssertIsTensor(self.new_features)
-        AssertTensorShapeEqual(self.initial_features, self.new_features)
-        return self.initial_features + self.new_features
-
-    def ControledWeightMult(self, features, weights):
-        AssertTensorDotDim(features, weights)
-        return K.dot(features, weights)
+        neigh_h = K.reshape(h_reshaped, (batch_size, max_neighbours, self.hidden_dim))

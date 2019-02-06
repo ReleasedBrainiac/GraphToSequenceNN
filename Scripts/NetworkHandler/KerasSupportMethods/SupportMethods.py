52981131660c748d28280e2edbb4ede83c962d94
Tobias Turke <Tobias.Turke@comparex.de>
2019-02-06 17:37:01 +0100

    changed some assert functions

--- Scripts/NetworkHandler/KerasSupportMethods/SupportMethods.py
+++ Scripts/NetworkHandler/KerasSupportMethods/SupportMethods.py
@@ -1,33 +1,42 @@
 from keras import backend as K
 
 def ConsoleReporter(label, printable,evaluate=True):
-    if evaluate and AssertIsTensor(printable):
+    AssertIsTensor(printable)
+    if evaluate:
         print(label,':\n',KerasEval(printable),'\n')
     else:
         print(label,':\n',printable,'\n')
         
 def AssertTensorShapeEqual(tensor_x, tensor_y):
-    assert (tensor_x.shape == tensor_y.shape), ('Assertion failed shapes dont match! [',tensor_x.shape,'] dont match " [',tensor_y.shape,']')
+    assert (tensor_x.shape == tensor_y.shape), ('Assertion failed shapes dont match! [',tensor_x.shape,'] dont match " [',tensor_y.shape,']')  
     
-def AssertVectorLength(vector_x, vector_y):
-    assert (vector_x.shape[0] == vector_y.shape[0]), ('Vector lenghts dont match! [',vector_x.shape[0],'] dont match " [',vector_y.shape[0],']')    
+def AssertNotNone(tensor, name):
+    assert (tensor is not None),('The given input',name,'was None!')
     
+def AssertNotNegative(value):
+    assert (value > -1), ('Input value was negative')
+
 def AssertTensorDotDim(tensor_x, tensor_y):
     AssertIsTensor(tensor_x)
     AssertIsTensor(tensor_y)
-    x_dim_0 = tensor_x.shape[0]
-    y_dim_1 = tensor_y.shape[1]
-    assert (x_dim_0 == y_dim_1),('Assertion failed for matrix multiplication cause [',x_dim_0,'] * [',y_dim_1,'] dont match!')
+    x_shape = tensor_x.shape
+    y_shape = tensor_y.shape
+    
+    if x_shape != y_shape:
+        x_dim = x_shape[1]
+        y_dim = y_shape[0]
+        assert (x_dim == y_dim),('Assertion failed for matrix multiplication cause [',x_dim,'] * [',y_dim,'] dont match!')
+
+def AssertAddTensorToTensor(tensor, add_tensor):
+    if tensor.shape != add_tensor.shape:
+        assert (tensor.shape[1] == add_tensor.shape[0]),("Weighted tensor can't be savely biased. This is caused by shape missmatch!")
+    
     
 def AssertIsTensor(tensor_x):
-    check = K.is_tensor(tensor_x)
-    assert (check),('Given input is no tensor! =>', tensor_x)
-    return check
+    assert (K.is_tensor(tensor_x)),('Given input is no tensor! =>', tensor_x)
 
 def AssertIsKerasTensor(tensor_x):
-    check = K.is_keras_tensor(tensor_x)
-    assert (check),('Given input is no KERAS tensor! =>', tensor_x)
-    return check
+    assert (K.is_keras_tensor(tensor_x)),('Given input is no KERAS tensor! =>', tensor_x)
 
 def KerasShape(tensor_x):
     AssertIsTensor(tensor_x)

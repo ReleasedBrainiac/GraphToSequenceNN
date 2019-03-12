8ef8423d0dd1da895aac4dcfdfe2523518b98edf
Tobias Turke <Tobias.Turke@comparex.de>
2019-03-12 14:23:08 +0100

    added class definition, documentation and fixed some calls and buggs.

--- Scripts/DatasetHandler/FileWriter.py
+++ Scripts/DatasetHandler/FileWriter.py
@@ -1,56 +1,54 @@
-from DatasetHandler.ContentSupport import isStr, isNone, isNotNone, isList
-from DatasetHandler.ContentSupport import setOrDefault
+from DatasetHandler.ContentSupport import isStr, isNotNone
+from DatasetHandler.ContentSupport import setOrDefault, AssertNotNone
 from Configurable.ProjectConstants import Constants
 
-class Writer:
-
-    # Variables init
+class Writer():
+    """
+    This class provides a FileWriter to store cleaned AMR Datasetpairs or stringyfied context.
+    """
     writer_encoding = 'utf8'
-    path = None
     context = None
     dataset_pairs = None
     out_path = None
+    
 
-    # Class init 
-    constants = Constants()
-
-    def __init__(self, input_path, in_output_extender='output',data_pairs=None, in_context=None):
+    def __init__(self, input_path:str, in_output_extender:str ='output',data_pairs:list =None, in_context:str =None):
         """
-        This is the constructor of the Writer class. 
+        This is the constructor of the (File-)Writer class. 
         Necessary is the input path only. 
         Extender for the output path provide a default value.
         To provide 2 types of input you can set amr datapairs or various context of type string.
         If both is present the data pairs wil be preferred.
-            :param input_path: path of the input file
-            :param in_output_extender='output': extender to create output file from input path
-            :param data_pairs=None: amr data pairs list like List<Array{sentence, semantic}>
-            :param in_context=None: optional if no data pairs present use context
+            :param input_path:str: path of the input file
+            :param in_output_extender:str: extender to create output file from input path
+            :param data_pairs:list: amr data pairs list like List<Array{sentence, semantic}>
+            :param in_context:str: optional if no data pairs present use context
         """   
         try:
-            if isNotNone(input_path) and isStr(input_path):
-                self.path = input_path
-                self.out_path = setOrDefault(self.path+'.'+ in_output_extender , self.constants.TYP_ERROR, isStr(in_output_extender))
+            self.constants = Constants()
+            self.path = input_path
 
-            if isNotNone(data_pairs) and isList(data_pairs):
+            AssertNotNone(self.path, msg='Given path for FileWriter was None!')
+            self.out_path = setOrDefault(self.path+'.'+ in_output_extender , self.constants.TYP_ERROR, isStr(in_output_extender))
+            print('Destination: ', self.out_path)
+
+            if isNotNone(data_pairs):
                 self.dataset_pairs = data_pairs
                 self.StoreAMR()
-
-            if isNotNone(in_context) and isStr(in_context) and isNone(data_pairs):
+            else:
+                AssertNotNone(in_context, msg='Given input for FileWriter was None!')
                 self.context = in_context
                 self.StoreContext()
-            
-            print('Destination: ', self.out_path)
 
         except Exception as ex:
             template = "An exception of type {0} occurred in [FileWriter.__init__]. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             print(message)
 
-    def SavingCorpus(self, data_pair):
+    def SavingCorpus(self, data_pair:list):
         """
         This function build a simple concatenation string containing a sentence and a semantic.
-            :param sentence: cleaned sentence with sentences flag
-            :param semantic: cleaned correspondign semantic for the sentence with semantic flag
+            :param data_pair:list: a list containing pairs of cleaned sentence with sentences flags and cleaned corresponding semantics with semantics flags.
         """
         try:
             if isNotNone(data_pair) and isStr(data_pair[0]) and isNotNone(data_pair[1]):

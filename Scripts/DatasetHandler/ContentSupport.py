bda0c0c230d1060a4e3fbfd697715b7b15a42693
Tobias Turke <Tobias.Turke@comparex.de>
2019-03-12 17:20:54 +0100

    moved function to file

--- Scripts/DatasetHandler/ContentSupport.py
+++ Scripts/DatasetHandler/ContentSupport.py
@@ -366,6 +366,23 @@ def GetRandomInt(min:int, max:int):
         print('WRONG INPUT FOR [GetRandomInt] so range [0,100] was used for generation!')
         return rnd.randint(0,100)
 
+def CalculateMeanValue(self, sentences_lengths:list):
+        """
+        This function calculate the mean over all values in a list.
+            :param sentences_lengths:list: lengths of all sentences
+        """
+        try:
+            sent_summ = 0
+
+            for index, _ in enumerate(sentences_lengths):
+                sent_summ += sentences_lengths[index]
+
+            mw = int(round(sent_summ / len(sentences_lengths)))
+            return mw
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [ContentSupport.CalculateMeanValue]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
 # Asserts for cases where an exception is necessary.
 def AssertNotNone(value, msg:str = ''):

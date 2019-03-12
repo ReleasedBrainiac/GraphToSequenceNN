df8c346e3e5eeb49c99b34e0d13594ccd67f7cdf
Tobias Turke <Tobias.Turke@comparex.de>
2019-03-12 17:46:40 +0100

    added documentation and data types. removed unused elements.

--- Scripts/GraphHandler/SemanticMatrixBuilder.py
+++ Scripts/GraphHandler/SemanticMatrixBuilder.py
@@ -2,41 +2,40 @@ from collections import OrderedDict
 from Configurable.ProjectConstants import Constants
 from DatasetHandler.ContentSupport import isNotEmptyString, getIndexedODictLookUp
 import numpy as np
-import sys
 
 class MatrixBuilder:
 
-    input_semantic = None
-    constants = None
-    graph_nodes = None
-    show_response = False
-    nodes_as_dict = False
-
-    def __init__(self, context, show_feedback=False):
+    def __init__(self, context:str =None, show_feedback:bool =False):
         """
         This class constructor stores the given context and allow to activation of showing the process results on the console.
-            :param context: amr input string
-            :param show_feedback: switch allows to show process response on console or not
+            :param context:str: amr input
+            :param show_feedback:bool: show process response on console
         """   
         try:
             self.input_semantic = context
             self.show_response = show_feedback
             self.constants = Constants()
             self.graph_nodes = OrderedDict()
+
+            #TODO this is optional implemented maybe remove it
+            self.nodes_as_dict = False
         except Exception as ex:
             template = "An exception of type {0} occurred in [SemanticMatricBuilder.Constructor]. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             print(message)
 
-    def MatrixSplitOnPrincipalDiagonal(self, edge_matrix):
+    def MatrixSplitOnPrincipalDiagonal(self, edge_matrix:np.ndarray):
         """
         This function split a whole [M x N] adjacent edge matrix of a graph into 2 [M x N] adjacent matrices.
         The 1. matrix will then contain all forward connections of origin edge matrix by zerofying all values under the principal diagonal. 
         The 2. matrix will contain the incomming connections of each node by transposing the 1. matrix.
+
         Attention: A directed graph will be threatened as a bidirected graph because we want to use it for text mapping, later. 
                    In this case there is no need to keep concrete graph directions.
+
         Additional: The algorith could use the numpy.matrix.transpose but it would cost more performance than creating both submatrices at once, so transpose is not used.
-            :param edge_matrix: numpy [M x N] edge matrix containing all forward and backward connections of a graph.
+
+            :param edge_matrix:np.ndarray: [M x N] edge matrix containing all forward and backward connections of a graph.
         """
         try:
             forward_connections = np.zeros(edge_matrix.shape)
@@ -57,25 +56,23 @@ class MatrixBuilder:
 
             stack = np.stack((forward_connections, backward_connections), axis=0)
             return stack
-
         except Exception as ex:
             template = "An exception of type {0} occurred in [SemanticMatricBuilder.MatrixSplitOnPrincipalDiagonal]. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             print(message)
             
-
-    def BuildEdgeMatrix(self, connections_dict, ordered_vertex_dict):
+    def BuildEdgeMatrix(self, connections:list, ordered_vertex:OrderedDict):
         """
-        This function build a edge matrix on a given connections list and return it with its corresponding vertex list.
-            :param connections_dict: dict of found connections
-            :param ordered_vertex_dict: ordered of collected verticies
+        This function build a edge matrix on a given connections list and return it with it's corresponding vertex list.
+            :param connections:list: found connections
+            :param ordered_vertex:OrderedDict: collected verticies
         """   
         try:
-            array_format = (len(ordered_vertex_dict), len(ordered_vertex_dict))
+            array_format = (len(ordered_vertex), len(ordered_vertex))
             edge_matrix = np.zeros(array_format)
-            indexed_verticies = getIndexedODictLookUp(ordered_vertex_dict)
+            indexed_verticies = getIndexedODictLookUp(ordered_vertex)
 
-            for edge in connections_dict:
+            for edge in connections:
                 start_index = indexed_verticies[edge[0]]
                 end_index = indexed_verticies[edge[1]]
                 edge_matrix[start_index, end_index] = 1
@@ -88,10 +85,10 @@ class MatrixBuilder:
             message = template.format(type(ex).__name__, ex.args)
             print(message)
 
-    def CollectNewNode(self, node_string):
+    def CollectNewNode(self, node_string:str):
         """
         This function collect amr node definitions from amr node string.
-            :param node_string: amr string fragment containing a node definition
+            :param node_string:str: amr string fragment containing a node definition
         """   
         try:
             parts = node_string.replace(' ', '').replace('\n', '').replace('(', '').replace(')', '').split('/')
@@ -117,7 +114,6 @@ class MatrixBuilder:
     def Execute(self):
         """
         This function execute the matrix building and vertex collecting process.
-        The whole implementation is inspired by stack processing.
         """   
         try:
             nodes_stack = []
@@ -146,8 +142,8 @@ class MatrixBuilder:
 
                     if prev_node != None: connections_list.append([prev_node, next_node])
             
-            edges, verticies = self.BuildEdgeMatrix(connections_list, self.graph_nodes)
-            edge_pair = self.MatrixSplitOnPrincipalDiagonal(edges)
+            edges, verticies = self.BuildEdgeMatrix(connections=connections_list, ordered_vertex=self.graph_nodes)
+            edge_pairs = self.MatrixSplitOnPrincipalDiagonal(edges)
 
             if self.show_response:
                 print('#####################################')
@@ -155,12 +151,12 @@ class MatrixBuilder:
                 print('Nodes:\n',self.graph_nodes)
                 print('Cons.:\n',connections_list)
                 print('Vertices:\n', verticies)
-                print('Edges:\n', edge_pair)
+                print('Edges:\n', edge_pairs)
             
             if self.nodes_as_dict:
-                return [edge_pair, self.graph_nodes]
+                return [edge_pairs, self.graph_nodes]
             else:
-                return [edge_pair, list(self.graph_nodes.values())]
+                return [edge_pairs, list(self.graph_nodes.values())]
 
         except Exception as ex:
             template = "An exception of type {0} occurred in [SemanticMatricBuilder.Execute]. Arguments:\n{1!r}"

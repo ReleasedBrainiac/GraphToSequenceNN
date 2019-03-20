237c3cc8f7aba12ab64d714d8be6c679b39fd678
Tobias Turke <Tobias.Turke@comparex.de>
2019-03-20 17:00:37 +0100

    removed deprecated prints

--- Scripts/NetworkHandler/Builder/ModelBuilder.py
+++ Scripts/NetworkHandler/Builder/ModelBuilder.py
@@ -294,7 +294,8 @@ class ModelBuilder():
         except Exception as ex:
             template = "An exception of type {0} occurred in [ModelBuilder.BuildGraphEmeddingConcatenation]. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
-            print(message) 
+            print(message)
+            raise
 
     def BuildGraphEmbeddingEncoder(
                             self, 

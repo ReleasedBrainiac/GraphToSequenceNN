2cf21728de39148e4acfce9990b5da5e332ea20b
ReleasedBrainiac <tobias.turke@gmx.net>
2019-07-01 12:57:55 +0200

    tets

--- Scripts/Plotter/PlotHistoFile.py
+++ Scripts/Plotter/PlotHistoFile.py
@@ -1,20 +1,25 @@
 import sys
+from DatasetHandler.FileReader import Reader
+
 
 class HistoryPlotter(object):
     """
-    This class provides a FileReader for text files.
+    This class provides a History file plotter using mathplot.
     """
 
-    ENCODING:str = sys.stdout.encoding or sys.getfilesystemencoding()
+    _context_lines:list = None
 
-    def __init__(self, histo_context:str = None):
+    def __init__(self, path:str = None):
         """
         The class constructor. 
-            :param histo_context:str: history context of the hitory file
+            :param path:str: path of the file containing the history
         """   
         try:
-            
-            self._histo_context = histo_context  if not (histo_context is None) else None           
+            self._path = path  if not (path is None) else None       
+            if sys.path.exists(path):
+                self._context_lines = Reader(path).Lines()
+            else:
+                print("File does not exist!")
         except Exception as ex:
             template = "An exception of type {0} occurred in [HistoryPlotter.Constructor]. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)

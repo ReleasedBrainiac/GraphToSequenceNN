01dab05ff50b4def4bf255b68ed5d98d28f88141
Tobias Turke <Tobias.Turke@comparex.de>
2019-08-11 15:18:35 +0200

    out commented print outs

--- Scripts/DatasetHandler/NumpyHandler.py
+++ Scripts/DatasetHandler/NumpyHandler.py
@@ -1,5 +1,6 @@
 import numpy as np
 import re
+import sys
 from DatasetHandler.ContentSupport import isNotNone, AssertNotNone, StatusReport, ConcatenateNdArrays, RepeatNTimsNdArray
 from Configurable.ProjectConstants import Constants
 
@@ -130,7 +131,7 @@ class NumpyDatasetPreprocessor():
         self._show_feedback:bool = show_feedback
         self._np_file_names:list = Constants().NP_TEACHER_FORCING_FILE_NAMES
 
-    def PreprocessTeacherForcingDS(self, nodes_embedding:np.ndarray, fw_look_up:np.ndarray, bw_look_up:np.ndarray, vecs_input_sentences:np.ndarray, vecs_target_sentences:np.ndarray, save:bool = False):
+    def PreprocessTeacherForcingDS(self, nodes_embedding:np.ndarray, fw_look_up:np.ndarray, bw_look_up:np.ndarray, vecs_input_sentences:np.ndarray, vecs_target_sentences:np.ndarray, save:bool = False, use_padded_vecs:bool = True):
         """
         This method preprocess the dataset to provide astructure for teacher forcing -> sentences will be split into words -> generate sample for each word instead each sentence.
             :param nodes_embedding:np.ndarray: node embedding numpy array
@@ -138,7 +139,8 @@ class NumpyDatasetPreprocessor():
             :param bw_look_up:np.ndarray: backward look up numpy array
             :param vecs_input_sentences:np.ndarray: input sentences vector numpy array -> they will be word level seperated
             :param vecs_target_sentences:np.ndarray: target sentences vector numpy array
-            :param save:bool=False: save result or not
+            :param save:bool: save result or not
+            :param use_padded_vecs:bool: the given sentence input and targets are only padded vecs not embedded
         """
         try:
             dataset_len = len(vecs_input_sentences)
@@ -150,17 +152,36 @@ class NumpyDatasetPreprocessor():
                 vecs_input_words = []
                 vecs_target_words = []
 
+                print("Shape Inp: ", vecs_input_sentences.shape, " ... [0]", vecs_input_sentences[0])
+                print("Shape Out: ", vecs_target_sentences.shape, " ... [0]",  vecs_target_sentences[0])
+
                 print("Start Dataset Generator")
                 for s_idx in range(dataset_len):
-                    tmp_vecs_input_words = np.trim_zeros(vecs_input_sentences[s_idx])
-                    tmp_vecs_target_words = np.trim_zeros(vecs_target_sentences[s_idx])
-                    qualified_entries = np.count_nonzero(tmp_vecs_input_words) - 1      #The -1 mean i will not include the the first copy since we keep the initial as well!
+
+                    if use_padded_vecs:
+                        tmp_vecs_input_words = np.trim_zeros(vecs_input_sentences[s_idx])
+                        tmp_vecs_target_words = np.trim_zeros(vecs_target_sentences[s_idx])
+                        qualified_entries = np.count_nonzero(tmp_vecs_input_words) - 1      #The -1 mean i will not include the the first copy since we keep the initial as well!
+                    else:
+                        tmp_vecs_input_words = vecs_input_sentences[s_idx]
+                        tmp_vecs_target_words = vecs_target_sentences[s_idx]
+                        qualified_entries = len(tmp_vecs_input_words) - 1
+
+                    #print("tmp_vecs_input_words: ", tmp_vecs_input_words.shape)
+                    #print("tmp_vecs_target_words: ", tmp_vecs_target_words.shape)
+                    #print("qualified_entries: ", qualified_entries)
 
                     nodes_emb.append(RepeatNTimsNdArray(times=qualified_entries, array=nodes_embedding[s_idx]))
                     forward_look_up.append(RepeatNTimsNdArray(times=qualified_entries, array=fw_look_up[s_idx]))
                     backward_look_up.append(RepeatNTimsNdArray(times=qualified_entries, array=bw_look_up[s_idx]))
-                    vecs_input_words.append(tmp_vecs_input_words.reshape((tmp_vecs_input_words.shape[0],))[:-1])
-                    vecs_target_words.append(tmp_vecs_target_words.reshape((tmp_vecs_target_words.shape[0],)))
+
+                    #print("Trimmed: ", tmp_vecs_input_words.reshape((tmp_vecs_input_words.shape[0],))[:-1])
+
+                    vecs_input_words.append( tmp_vecs_input_words.reshape((tmp_vecs_input_words.shape[0],))[:-1] )
+
+
+                    vecs_target_words.append( tmp_vecs_target_words.reshape((tmp_vecs_target_words.shape[0],)) )
+
                     if (self._show_feedback): StatusReport(run_index=s_idx, max_index=dataset_len, steps=500)
 
                 nodes_emb = ConcatenateNdArrays(nodes_emb)
@@ -173,6 +194,7 @@ class NumpyDatasetPreprocessor():
                     NumpyDatasetHandler(path=(self._folder_path + self._np_file_names[0])).Save3DNdArrayToTxt(array=nodes_emb)
                     NumpyDatasetHandler(path=(self._folder_path + self._np_file_names[1])).Save3DNdArrayToTxt(array=forward_look_up)
                     NumpyDatasetHandler(path=(self._folder_path + self._np_file_names[2])).Save3DNdArrayToTxt(array=backward_look_up)
+
                     NumpyDatasetHandler(path=(self._folder_path + self._np_file_names[3])).Save2DNdArrayToTxt(array=vecs_input_words)
                     NumpyDatasetHandler(path=(self._folder_path + self._np_file_names[4])).Save2DNdArrayToTxt(array=vecs_target_words)
 
@@ -197,6 +219,7 @@ class NumpyDatasetPreprocessor():
         """
         try:
             assert (len(nodes_embedding) == len(fw_look_up) == len(bw_look_up) == len(vecs_input_words) == len(vecs_target_words)), "The given inputs of NetworkInputPreparation aren't machting at first dimension!"
+            assert (len(nodes_embedding) >= split_border), ("The split index value was to high! [", len(nodes_embedding), " >= ", split_border, "]")
 
             train_x = [ nodes_embedding[:split_border], 
                         fw_look_up[:split_border], 

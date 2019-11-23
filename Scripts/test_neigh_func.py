71a9ab4918ec4c94168c47bd22a09dc038f70ce2
ReleasedBrainiac <tobias.turke@gmx.net>
2019-11-23 12:15:41 +0100

    edited test class

--- Scripts/test_neigh_func.py
+++ Scripts/test_neigh_func.py
@@ -1,6 +1,6 @@
 import numpy as np
 import keras.backend as K
-from keras.layers import Input
+from keras.layers import Lambda, Embedding, LSTM, concatenate,GRU
 import time
 from NetworkHandler.Neighbouring.NeighbourhoodCollector import Neighbourhood as Nhood
 from NetworkHandler.Builder import ModelBuilder
@@ -32,9 +32,13 @@ class TestNeigh():
 
         # From = > https://www.tensorflow.org/tutorials/text/nmt_with_attention
 
-
-        dummy_hidden = K.zeros((32, 400))
-        dummy_outs = K.zeros((32, 1, 400))
+        vocab_size = 15000
+        embedding_dim = 400
+        words = 64
+        batch_size = 32
+        dummy_hidden = K.zeros((32, embedding_dim))
+        dummy_outs = K.zeros((32, 16, embedding_dim))
+        embeddings = K.zeros((32, words))
 
         print ('Encoder output shape: (batch size, sequence length, units) {}'.format(dummy_outs.shape))
         print ('Encoder hidden state shape: (batch size, units) {}'.format(dummy_hidden.shape))
@@ -43,12 +47,27 @@ class TestNeigh():
         mb = ModelBuilder.ModelBuilder( input_enc_dim = 1, 
                                         edge_dim = 1, 
                                         input_dec_dim = 1, 
-                                        batch_size = 64)
+                                        batch_size = batch_size)
 
-        attention_result, attention_weights = mb.BuildBahdanauAttentionPipe(10, dummy_outs, dummy_hidden)
+        context_vector, attention_weights = mb.BuildBahdanauAttentionPipe(embedding_dim, dummy_outs, dummy_hidden)
+        print("Attention result shape 1: (batch size, units) {}".format(context_vector.shape))
+        print("Attention weights shape 1: (batch_size, sequence_length, 1) {}".format(attention_weights.shape))
 
-        print("Attention result shape: (batch size, units) {}".format(attention_result.shape))
-        print("Attention weights shape: (batch_size, sequence_length, 1) {}".format(attention_weights.shape))
+        #context_vector = Lambda(lambda q: K.expand_dims(q, axis=1), name="attention_reshape")(context_vector)
+        #print("Attention result shape 2: (batch size, units) {}".format(context_vector.shape))
+
+        embedding = Embedding(vocab_size, embedding_dim)(embeddings)
+        print("Embedding result shape 1: (batch size, units) {}".format(embedding.shape))
+        print("Embedding act units: ", context_vector.shape[-1].value)
+
+        #embedding = LSTM(context_vector.shape[-1].value, batch_size=batch_size, return_sequences=True, name="attention_activate")(embedding)
+        print("Embedding result shape 2: (batch size, units) {}".format(embedding.shape))
+
+        context_vector = Lambda(lambda q: K.expand_dims(q, axis=1), name="attention_reshape")(context_vector)
+        print("Attention result shape 2: (batch size, units) {}".format(context_vector.shape))
+
+        stated_att_encoder = concatenate([context_vector,embedding], name="att_emb_concatenation", axis=-1)
+        print("Stated Encoder result shape: (batch size, units) {}".format(stated_att_encoder.shape))
 
 if __name__ == "__main__":
     TestNeigh().TestBahdanau()
